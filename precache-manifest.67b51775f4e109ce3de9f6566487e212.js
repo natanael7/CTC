e5e48519c59446d796d52c5a58f6dc6cd9b1fceb
Natanael Marian <mac@Natanael-MacBookAir.station>
2019-06-24 15:08:44 +0300

    Updates

---
+++ precache-manifest.67b51775f4e109ce3de9f6566487e212.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b440ec6836736cb3d99004c7d0dfbcc",
+    "url": "/ctc/index.html"
+  },
+  {
+    "revision": "9da978b17d04bb26a5fb",
+    "url": "/ctc/static/js/2.65263247.chunk.js"
+  },
+  {
+    "revision": "6d37e5634939afff3561",
+    "url": "/ctc/static/js/main.e3fef13d.chunk.js"
+  },
+  {
+    "revision": "7550e33ccca5765a96d1",
+    "url": "/ctc/static/js/runtime~main.baac4c37.js"
+  }
+]);
